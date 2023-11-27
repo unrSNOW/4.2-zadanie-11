8cef3fbd380db5e70401b0847b1eef41beb90c0b
unrSNOW <Yaroslav.sapon@bk.ru>
2023-11-27 13:01:20 +0300

    Добавьте файлы проекта.

---
+++ 4.2 zadanie 11/Program.cs
@@ -0,0 +1,33 @@
+﻿int[,] matrix = 
+    {
+            { 3, 7, 2 },
+            { 8, 5, 1 },
+            { 4, 6, 9 }
+     };
+
+int row= matrix.GetLength(0);
+int col = matrix.GetLength(1);
+
+int[] min = new int[row];
+for (int i = 0; i < row; i++)
+{
+    int minValue = int.MaxValue;
+    for (int j = 0; j < col; j++)
+    {
+        if (matrix[i, j] < minValue)
+        {
+            minValue = matrix[i, j];
+        }
+    }
+    min[i] = minValue;
+}
+int maxIndex = 0;
+for (int i = 0; i < row; i++)
+{
+    if (min[i] > min[maxIndex])
+    {
+        maxIndex = i;
+    }
+}
+Console.WriteLine($"Индексы элемента с найденным значением: ({maxIndex})");
+
